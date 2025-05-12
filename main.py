cda93d74ed8d28a0079ef050f9ea8593886a3625
wh0syx <wh0syx@proton.me>
2025-05-12 10:20:01 +0200

    Testing deploy

--- main.py
+++ main.py
@@ -136,8 +136,3 @@ def delete_product(name: str, db: Session = Depends(get_db)):
         raise HTTPException(404, "Product not found")
     db.delete(product)
     db.commit()
-
-# --- ENTRY POINT ---
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run("main:api", host="127.0.0.1", port=8000, reload=True)
